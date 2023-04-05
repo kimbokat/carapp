fe875d9e36e6f3d1c00992fbc60c19d83916bb0b
kimbokat <90966436+kimbokat@users.noreply.github.com>
2023-04-05 13:53:38 +0300

    added functionality to add a car

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import Carlist from './components/Carlist';
 
 function App() {
   return (
     <div className="App">
+      <AppBar position="static">
+      <Toolbar>
+      <Typography variant="h6">
+
+      </Typography>
+
+      </Toolbar>
+      </AppBar>
      <Carlist />
     </div>
   );
